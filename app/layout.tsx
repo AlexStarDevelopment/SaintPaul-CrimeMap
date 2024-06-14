06be342b21490e6da1590a01cc2592c46af67411
Alex Star <astar@intertech.com>
2024-06-14 11:27:11 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="dark">
+    <html lang="en" data-theme="emerald">
       <head>
         <link
           rel="stylesheet"
