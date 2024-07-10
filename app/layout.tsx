25220b8b7aea0e3ed5ab0c2a98bb395f044e4062
Alex Star <astar@intertech.com>
2024-07-10 15:37:30 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,11 @@ export default function RootLayout({
         `}
         </Script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
