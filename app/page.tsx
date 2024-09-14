be3ada7b624c174721295e26e52956a2aa8d3799
Alex Star <alexstar1408@gmail.com>
2024-09-14 15:22:34 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -214,6 +214,7 @@ export default function Home() {
       <div className="card w-[100vw] bg-primary text-primary-content m-5">
         <div className="card-body items-center text-center">
           <h3 className="card-title">Change Log</h3>
+          <p className="m-5">9/14/24 1.8.1 - August data added</p>
           <p className="m-5">8/8/24 1.8.1 - Second half of July data added</p>
           <p className="m-5">7/22/24 1.8.1 - Data fixes for university ave</p>
           <p className="m-5">
